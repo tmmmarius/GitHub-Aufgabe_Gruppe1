559ea0f71bbf7ae47b673fcfe12a0a8665fdbb0d
markuslangmann <markus.langmann@tu-dortmund.de>
2023-02-23 06:33:05 +0100

    Update helper.R
    getData(): Zu untersuchenden Daten als Data-Frame einlesen

--- helper.R
+++ helper.R
@@ -1,1 +1,10 @@
-
+getData = function(str = FALSE){
+  
+  #Struktur des Datensatzes auf der Konsole ausgeben
+  if(str == TRUE){
+    str(data)
+  }
+  
+  return(read.csv2("daten.csv", header = TRUE))
+  
+}
