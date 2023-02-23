49adddad74985b60cd48e65fa6818b103906785e
markuslangmann <markus.langmann@tu-dortmund.de>
2023-02-23 06:45:04 +0100

    Update auswertung.R
    - helper.R laden
    - Funktionsaufruf um Daten in Workspace zu laden
    - Funktion zur Auswertung metrischer Variablen begonnen

--- auswertung.R
+++ auswertung.R
@@ -1,1 +1,19 @@
+#Enthaelt Funktionen fuer die statistische Auswertung
+source("helper.R") #Hilfsfunktionen
 
+##Daten laden
+#Zugriff auf die Variablen wie folgt: daten$VARIABLENNAME
+#Durch setzen des Arguments str = TRUE wird zusaetzlich die Struktur beim Laden  
+#auf der Konsole ausgegeben
+daten = getData(str = TRUE) 
+
+
+#Berechnung und Ausgabe von Statistiken fuer metrische Variablen
+metrischeVariablen = function(x){
+  
+  mittelwert = mean(x)
+  
+  
+  return(list("arithmetisches Mittel" = mittelwert))
+}
+metrischeVariablen(daten$Alter)
