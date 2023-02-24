01adb39b9a005eaa428134ef64245382c245d60e
samueltrippler <126190187+samueltrippler@users.noreply.github.com>
2023-02-24 19:43:03 +0100

    Update funktionen.R

--- funktionen.R
+++ funktionen.R
@@ -7,3 +7,13 @@ metrischeVariablen = function(x){
   return(list("arithmetisches Mittel" = mittelwert))
 }
 metrischeVariablen(daten$Alter)
+
+# 3e) Quantilbasiert kategorisiert von ordinal skalierter Variable
+qk <- function(x){
+  Kategorie <- 1:length(x)
+  Kategorie[which(x < 3)] <- "niedrig"
+  Kategorie[which(x > 2 & x < 6)] <- "mittel"
+  Kategorie[which(x > 5)] <- "hoch"
+  
+  return(list("Kategorisiert" = Kategorie))
+}
